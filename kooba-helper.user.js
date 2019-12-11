eaa395323a866ae6dc5dbcd9437d1a29d940b0b1
[Shrek] <shrek+github@shrekis.love>
2019-12-10 23:45:57 -0700

    update test

--- kooba-helper.user.js
+++ kooba-helper.user.js
@@ -3,7 +3,7 @@
 // @namespace    kooba-helper@shrek
 // @description For a better kooba<=>abook experience. This adds search links to Abook forums code boxes.
 // @author Shrek
-// @version 2019.12.10.6
+// @version 2019.12.10.7
 // @updateURL https://shrekislovelife.github.io/kooba-helper/kooba-helper.meta.js
 // @downloadURL https://shrekislovelife.github.io/kooba-helper/kooba-helper.user.js
 // @supportURL https://abook.link/book/index.php?topic=54768
