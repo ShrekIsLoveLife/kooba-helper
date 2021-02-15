596e4b80a3071d43e8d1c1ad275a5b655152aafd
[Shrek] <shrek+github@shrekis.love>
2021-02-15 12:37:35 -0500

    binsearch other groups and abook options

--- kooba-helper.user.js
+++ kooba-helper.user.js
@@ -3,7 +3,7 @@
 // @namespace    kooba-helper@shrek
 // @description For a better kooba<=>abook experience. This adds search links to Abook forums code boxes.
 // @author Shrek, rhymesagainsthumanity, pushr (original creator)
-// @version 2020.04.26.1
+// @version 2021.02.15.1
 // @updateURL https://shrekislovelife.github.io/kooba-helper/kooba-helper.meta.js
 // @downloadURL https://shrekislovelife.github.io/kooba-helper/kooba-helper.user.js
 // @supportURL https://abook.link/book/index.php?topic=54768
@@ -31,6 +31,13 @@ const indexers = [
   },
   {
     name: 'BinSearch',
+    url: 'https://DoNotRefer.Me/#https://www.binsearch.info/?q=Traitors+of+Rome&max=1000&&adv_age=&adv_sort=date&server=2&&q={query}',
+    codeFn: function(code) {
+      return sanatize_common(code);
+    }
+  },
+  {
+    name: 'BinSearch-Abook',
     url: 'https://DoNotRefer.Me/#https://www.binsearch.info/?max=100&adv_g=alt.binaries.mp3.abooks&adv_age=&adv_sort=date&q={query}',
     codeFn: function(code) {
       return sanatize_common(code);
